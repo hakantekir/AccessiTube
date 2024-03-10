a67cea47703711ef8f673ae5baf7365d9d0500a1
Hakan Tekir <hakantekir2002@outlook.com>
2024-03-10 21:33:12 +0300

    Initial Commit

---
+++ AccessiTube/AccessiTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessiTubeApp.swift
+//  AccessiTube
+//
+//  Created by Hakan Tekir on 6.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessiTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
